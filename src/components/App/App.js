3b1b6f5b3777db390c754b7ab2cd50c666f19be4
Felix <56250324+FelixHultberg@users.noreply.github.com>
2020-12-29 17:58:19 +0100

    Revert "Route"

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,16 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
-
-// Layouts
-import MainLayout from '../../layouts/MainLayout'
-import LoginLayout from '../../layouts/LoginLayout'
-
-// Components
 import Dashboard from '../Dashboard/Dashboard'
 import Login from '../Login/Login'
 import PrivateRoute from '../PrivateRoute/PrivateRoute'
 import ForgotPassword from '../ForgotPassword/ForgotPassword'
 import UpdateProfile from '../UpdateProfile/UpdateProfile'
-import Services from '../Services/Services'
 
-import { AuthProvider } from '../../contexts/AuthContext'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { AuthProvider } from '../../contexts/AuthContext';
 
 function App() {
   return (
@@ -25,32 +18,10 @@ function App() {
       <div>
         <AuthProvider>
           <Switch>
-            <PrivateRoute exact path="/" render={() => (
-              <MainLayout>
-                <Dashboard />
-              </MainLayout>
-            )} />
-            <PrivateRoute path="/update-profile" render={() => (
-              <MainLayout>
-                <UpdateProfile />
-              </MainLayout>
-            )} />
-            <PrivateRoute path="/services" render={() => (
-              <MainLayout>
-                <Services />
-              </MainLayout>
-            )} />
-            
-            <Route path="/login" render={() => (
-              <LoginLayout>
-                <Login />
-              </LoginLayout>
-            )} />
-            <Route path="/forgot-password" render={() => (
-              <LoginLayout>
-                <ForgotPassword />
-              </LoginLayout>
-            )} />
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
           </Switch>
         </AuthProvider>
       </div>
@@ -58,4 +29,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
