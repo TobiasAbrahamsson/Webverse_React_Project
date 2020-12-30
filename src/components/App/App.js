32b54e07d8189e9db3a44c531cf33b41b3adc711
Felix Hultberg <felix.hultberg@medieinstitutet.se>
2020-12-30 12:02:51 +0100

    boiii

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,14 +6,8 @@ import {
 } from 'react-router-dom'
 
 // Layouts
-<<<<<<< HEAD
 import MainLayout from '../../layouts/MainLayout'
 import LoginLayout from '../../layouts/LoginLayout'
-=======
-//import MainLayout from '../../layouts/MainLayout'
-//import LoginLayout from '../../layouts/LoginLayout'
-//import ProjectLayout from '../../layouts/ProjectLayout'
->>>>>>> b25f5c99104a0708e5f28c8cd5660fc0b812ad2f
 
 // Components
 import Dashboard from '../Dashboard/Dashboard'
@@ -32,7 +26,6 @@ function App() {
       <div>
         <AuthProvider>
           <Switch>
-<<<<<<< HEAD
             <PrivateRoute exact path="/" render={() => (
               <MainLayout>
                 <Dashboard />
@@ -59,18 +52,6 @@ function App() {
                 <ForgotPassword />
               </LoginLayout>
             )} />
-=======
-
-            <PrivateRoute exact path="/" component={Dashboard} />
-            <PrivateRoute exact path="/account" component={Account} />
-
-            <PrivateRoute exact path="/:id/overview" component={Overview} />
-            <PrivateRoute exact path="/:id/services" component={Services} />
-
-            <Route path="/login" component={Login} />
-            <Route path="/forgot-password" component={ForgotPassword} />
-            
->>>>>>> b25f5c99104a0708e5f28c8cd5660fc0b812ad2f
           </Switch>
         </AuthProvider>
       </div>
